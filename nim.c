404a96a2b70c4f47198d4346291f1e6ce982ea39
mbertolino <mattias.bertolino@gmail.com>
2017-03-20 13:20:16 +0100

    Added quasi p-player

--- nim.c
+++ nim.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 
 void print_board(int N_rows, int *rows) {
 	for (int i = 0; i < N_rows; i++) {
@@ -18,7 +18,6 @@ int main(int argc, char *argv[]) {
     printf("Expects 2 arguments, # rows and sticks\n");
     return -1;
   }
-  
   int N_rows = atoi(argv[1]);
   int sticks = atoi(argv[2]);
   int total_sticks = N_rows*sticks;
@@ -33,8 +32,12 @@ int main(int argc, char *argv[]) {
     rows[i] = sticks;
   }
   
+  double p = 0.4;
   int player = 2;
   int row, pick;
+  int X;
+  
+  srand(time(NULL));
   
   while (1) {
     player = 3 - player;
@@ -43,26 +46,51 @@ int main(int argc, char *argv[]) {
     print_board(N_rows, rows);
     printf("Player %i:s turn: \n", player);
     
-    while (1) {
-      int ret = scanf("%i%i", &row, &pick); // cannot read strings
-      
-      if (ret <= 0) {
-        printf("Illegal input: Enter numbers plz. Try again.\n");
-        print_board(N_rows, rows);
-        continue;
+    if (player == 1) {
+      while (1) {
+        int ret = scanf("%i%i", &row, &pick); // cannot read strings
+        row--;
+        if (ret <= 0) {
+          printf("Illegal input: Enter numbers plz. Try again.\n");
+          print_board(N_rows, rows);
+          continue;
+        }
+        if (row > N_rows-1 || row < 0) {
+          printf("Illegal move: Rows out of bounds. Try again.\n");
+          print_board(N_rows, rows);
+          continue;
+        } else if (pick < 1 || pick > rows[row]) {
+          printf("Illegal move: Wrong number of sticks. Try again.\n");
+          print_board(N_rows, rows);
+          continue;
+        }
+        break;
       }
-      if (row > N_rows || row < 1) {
-        printf("Illegal move: Rows out of bounds. Try again.\n");
-        print_board(N_rows, rows);
-        continue;
-      } else if (pick < 1 || pick > rows[row-1]) {
-        printf("Illegal move: Wrong number of sticks. Try again.\n");
-        print_board(N_rows, rows);
-        continue;
+    } else {
+      X = 0;
+      row = 1;
+      for (int i = 0; i < N_rows; i++)
+        X ^= rows[i];
+      if (X == 0 || p < (double)rand()/RAND_MAX) {
+        do {
+          row = (double)N_rows*rand()/RAND_MAX;
+          printf("row = %i\n", row);
+        } while (rows[row] == 0);
+      pick = 1 + (double)rows[row]*rand()/RAND_MAX;
+      printf("pick = %i\n", pick);
+      } else {
+        for (int i = 0; i < N_rows; i++) {
+          if ((X^rows[i]) < rows[i]) {
+            row = i+1;
+            pick = rows[i] - (X^rows[i]);
+            break;
+          }
+        }
       }
-      break;
     }
-    rows[row-1] -= pick;
+    
+    
+    rows[row] -= pick;
     
     total_sticks -= pick;
     if (total_sticks <= 0) {
